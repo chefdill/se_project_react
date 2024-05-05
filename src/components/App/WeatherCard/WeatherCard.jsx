ff1ae4fba0c00c9c6180fb7005c19bdce1383520
chef_dill <dillon.sareerat@gmail.com>
2024-05-05 15:59:04 -0500

    WeatherCard Basics

---
+++ src/components/App/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,15 @@
+import sunny from "../../../assets/sunCard.png";
+import './WeatherCard.css';
+
+function WeatherCard () {
+    return <section className="weather-card">
+        <p className="weather-card__temp">75 &deg; F</p>
+        <img 
+        src={sunny}
+        alt="" 
+        className="weather-card__img" 
+        />
+    </section>;
+}
+
+export default WeatherCard;
