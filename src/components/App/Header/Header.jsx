3c1b966b62a97a4b6da3741977dc2ecabc77374e
Dillon Sareerat <118320008+chefdill@users.noreply.github.com>
2024-05-05 00:53:55 -0500

    assets not getting imported

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,6 +1,25 @@
+import './Header.css';
+import wtwr from '../../assets/wtwr.svg';
+import avatar from '../../assets/avatar.png';
+
 function Header() {
     return (
-        <header>HEADER</header>
+        <header className="header">
+            <img 
+            src={wtwr} 
+            alt="Logo" 
+            className="header__logo"/>
+            <p className="header__date-and-location"> DATE, LOCATION </p>
+            <button className="header__add-clothes-btn"> + Add Clothes</button>
+            <div className="header_-user-container">
+                <p className="header__username">NAME</p>
+                <img 
+                src={avatar} 
+                alt="Avatar" 
+                className="header__avatar" />
+            </div>
+
+        </header>
     )
 }
 
