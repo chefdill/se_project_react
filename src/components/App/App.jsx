63fa0a24f82b4f8dac29ebba47170be244e7dbbf
Dillon Sareerat <118320008+chefdill@users.noreply.github.com>
2024-05-04 23:44:26 -0500

    Header is added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 
 function App() {
   return 
- <div className="page"></div>
+ <div className="page">
+  <div className="page__content">Hello</div>
+ </div>
 }
 
 export default App
