b76f4498a85138c81a76c2c4e1eee7f4737bc308
chef_dill <dillon.sareerat@gmail.com>
2024-05-05 21:42:35 -0500

    cards

--- src/components/App/Main/Main.jsx
+++ src/components/App/Main/Main.jsx
@@ -1,6 +1,9 @@
 import WeatherCard from '../WeatherCard/WeatherCard.jsx';
+import ItemCard from '../ItemCard/ItemCard';
+import { defaultClothingItems } from '../../../utils/constants.js';
+import './Main.css';
 
-function Main () {
+function Main ({ weatherData }) {
 return(
     <main>
     <WeatherCard />
@@ -8,7 +11,14 @@ return(
         <p className="cards__text">
             Today is 75 &deg; F / You may want to wear:
         </p>
-        {/* {TODO - add the cards} */}
+        <ul className="cards__list">
+            {defaultClothingItems.filter((item) => {
+                return item.weather === weatherData.type;
+            })
+            .map((item) => {
+                return <ItemCard key={item._id} item={item} />;
+              })}
+        </ul>
         </section>
     </main>
     );
