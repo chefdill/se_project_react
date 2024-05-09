957f6fec5d01b27f3b81b289e946293c37c16b7d
Dillon Sareerat <118320008+chefdill@users.noreply.github.com>
2024-05-09 18:15:20 -0500

    current update

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -7,12 +7,16 @@ const Header = ({ onCreateModal, location }) => {
     const currentDate = new Date().toLocaleString('default', {month: 'long', day: 'numeric' });
     return (
         <header className="header">
-            <img 
-            src={wtwr} 
-            alt="Logo" 
-            className="header__logo"/>
+                <img 
+                src={wtwr} 
+                alt="Logo" 
+                className="header__logo"/>
             <p className="header__date-and-location"> {currentDate}, {location} </p>
-            <button className="header__add-clothes-btn" onClick={onCreateModal}> + Add Clothes</button>
+            <button 
+                className="header__add-clothes-btn" 
+                onClick={onCreateModal}> 
+                + Add Clothes
+            </button>
             <div className="header__user-container">
                 <p className="header__username">Terrence Tegegne</p>
                 <img 
