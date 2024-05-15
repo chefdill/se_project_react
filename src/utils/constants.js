ac70b58a5dc2442129331f90cce03a39c6d9652f
chef_dill <dillon.sareerat@gmail.com>
2024-05-14 20:58:29 -0500

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
     }
   ]
+
+  export const coordinates = {
+    latitude: 30.267153,
+    longitude: -97.743057
+  };
+
+  export const APIkey = "a544def3c184a59b46200045057eb068";
