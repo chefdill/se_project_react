bf367d31dc643332d969bcf22190fa8945136c16
Dillon Sareerat <118320008+chefdill@users.noreply.github.com>
2024-05-15 19:46:27 -0500

    return fetch

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,5 +1,5 @@
 export const getWeather = ({ latitude, longitude }, APIkey) => {
-  fetch(  
+  return fetch(  
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=
     ${longitude}&units=imperial&appid=${APIkey}`
     ).then((res) => {
