2fe9fa48ec5221c794c1706f44a784b65af0b737
chef_dill <dillon.sareerat@gmail.com>
2024-07-07 18:37:59 -0500

    fixed context

--- src/components/CurrentTemperatureUnitContext.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const CurrentTemperatureUnitContext = React.createContext({
-    currentTemperatureUnit: "",
-    handleToggleSwitchChange: () => {}
-});
-
-// export default CurrentTemperatureUnitContext;
