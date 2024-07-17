b711e20856e88916cb438cda0cd88f4cf236477a
Dillon Sareerat <118320008+chefdill@users.noreply.github.com>
2024-07-17 01:01:54 -0500

    created api.js

---
+++ src/utils/api.js
@@ -0,0 +1,42 @@
+class Api {
+    constructor({ baseUrl, headers }) {
+      // constructor body
+      this.baseUrl = baseUrl;
+      this._headers = headers;
+    }
+  
+    _checkResponse(res) {
+      if (res.ok) {
+        return res.json();
+      }
+      // if the server returns an error, reject the promise
+      return Promise.reject(`Error ${res.status}`);
+    }
+  
+    getCards() {
+      return fetch(this.baseUrl + "/items", {
+        method: "GET",
+      }).then(this._checkResponse);
+    }
+  
+    addItem({ name, imageUrl, weather }) {
+      return fetch(this.baseUrl + "/items", {
+        method: "POST",
+        headers: this._headers,
+        body: JSON.stringify({
+          name,
+          imageUrl,
+          weather,
+        }),
+      }).then(this._checkResponse);
+    }
+  
+    deleteItem(id) {
+      return fetch(this.baseUrl + "/items/" + id, {
+        method: "DELETE",
+        headers: this._headers,
+      }).then(this._checkResponse);
+    }
+  }
+  
+  export default Api;
